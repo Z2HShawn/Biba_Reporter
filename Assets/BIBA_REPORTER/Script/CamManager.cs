cf86142db7bcb8bcd670b5d3ac91719751e1bad6
Shawn <titanseer1987@hotmail.com>
2015-03-16 10:53:05 -0700

    First Push
    Update:
    1.add the biba_reporter prototype

---
+++ Assets/BIBA_REPORTER/Script/CamManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CamManager : MonoBehaviour 
+{
+	public WebCamTexture 	webCamTex;
+	public WebCamDevice[]  	webCamDevice;
+	//public MeshRenderer 	meshRender;
+	public RawImage 		rawImage;
+
+	// Use this for initialization
+	void Start () 
+	{
+		InitialCam();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		 
+	}
+
+	void InitialCam()
+	{
+		webCamDevice 	= WebCamTexture.devices; //get cam device
+		webCamTex 		= new WebCamTexture(webCamDevice[0].name,500,500,24); 
+		webCamTex.Play();
+
+		rawImage = GetComponent<RawImage>();
+		rawImage.texture = webCamTex;
+		//meshRender 		= GetComponent<MeshRenderer>();
+		//meshRender.material.mainTexture = webCamTex;
+	}
+
+	public void EnableTex()
+	{
+		//meshRender.enabled = true;
+		webCamTex.Play();
+	}
+
+	public void DisableTex()
+	{
+		//meshRender.enabled = false;
+		webCamTex.Stop();
+	}
+}
---
+++ Assets/BIBA_REPORTER/Script/MicrophoneInput.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class MicrophoneInput : MonoBehaviour {
+	public float sensitivity = 100;
+	public float loudness = 0;
+
+	void Start() 
+	{
+		/*
+		if ( iPhone.generation == iPhoneGeneration.iPodTouch5Gen )
+		{
+			sensitivity = sensitivity * 1.5f;
+		};
+		*/
+		audio.clip = Microphone.Start(null, true, 3, 44100);
+		audio.loop = true; // Set the AudioClip to loop
+		audio.mute = true; // Mute the sound, we don't want the player to hear it
+		while (!(Microphone.GetPosition(null) > 0)){} // Wait until the recording has started
+		audio.Play(); // Play the audio source!
+	}
+	
+	void Update()
+	{
+		loudness = GetAveragedVolume() * sensitivity;
+	}
+	
+	float GetAveragedVolume()
+	{ 
+		float[] data = new float[256];
+		float a = 0;
+		audio.GetOutputData(data,0);
+		foreach(float s in data)
+		{
+			a += Mathf.Abs(s);
+		}
+		return a/256;
+	}
+
+	public void StopMicrophone()
+	{
+		Microphone.End(null);
+	}
+}
